2455f491c0bebc057e190ce751d1b1035b4507ad
Yoanna <yoansiyotova@gmail.com>
2020-05-04 20:51:56 +0300

    Add database

---
+++ CinemaReservationSystem/database/create_tables.py
@@ -0,0 +1,43 @@
+CREATE_USERS = '''
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY,
+        username VARCHAR(50) NOT NULL,
+        password VARCHAR(255)
+    );
+'''
+
+CREATE_MOVIES = '''
+    CREATE TABLE IF NOT EXISTS movies(
+        id INTEGER PRIMARY KEY,
+        name VARCHAR(50),
+        rating REAL
+    );
+'''
+
+CREATE_PROJECTION = '''
+    CREATE TABLE IF NOT EXISTS projections(
+        id INTEGER PRIMARY KEY,
+        movie_id INTEGER,
+        type VARCHAR(5),
+        date VARCHAR(20),
+        time VARCHAR(10),
+        FOREIGN KEY(movie_id) REFERENCES movies(id)
+    );
+'''
+CREATE_RESERVATION = '''
+    CREATE TABLE IF NOT EXISTS reservations(
+        id INTEGER PRIMARY KEY,
+        user_id INTEGER,
+        projection_id INTEGER,
+        row INTEGER,
+        col INTEGER,
+        FOREIGN KEY(user_id) REFERENCES users(id),
+        FOREIGN KEY(projection_id) REFERENCES projection(id),
+    );
+'''
+
+CREATE_SESSION = '''
+    CREATE TEMP TABLE IF NOT EXISTS session(
+        username VARCHAR(50)
+    );
+'''
