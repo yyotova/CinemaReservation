53dbfa202e49f7f4be9668964099274ce8de5984
PetarDamyanov <Petar.A.Damyanov@gmail.com>
2020-05-12 12:57:48 +0300

    atomicmethod

--- cinema_reservation_system/users/users_gateway.py
+++ cinema_reservation_system/users/users_gateway.py
@@ -2,15 +2,15 @@ from cinema_reservation_system.database.db import Database
 import cinema_reservation_system.users.models
 from cinema_reservation_system.database.user_specific.user_manipulation import *
 from cinema_reservation_system.database.create_tables import *
-from cinema_reservation_system.decorators import atomic
+from cinema_reservation_system.decorators import atomicmethods
 
 
+@atomicmethods
 class UserGateway:
     def __init__(self):
         self.model = cinema_reservation_system.users.models.UserModel
         self.db = Database()
 
-    @atomic
     def create(self, cursor, *, email, password, salt):
         try:
             cursor.execute(INSERT_USER, (email, password, salt))
@@ -18,11 +18,9 @@ class UserGateway:
         except Exception as e:
             return e
 
-    @atomic
     def login(self, cursor, *, email, password):
         return cursor.execute(SELECT_USER, (email, password)).fetchone()
 
-    @atomic
     def user(self, cursor, *, email):
         cursor.execute('''
             SELECT id
@@ -32,15 +30,12 @@ class UserGateway:
 
         return cursor.fetchone()[0]
 
-    @atomic
     def get_salt(self, cursor, salt):
         if cursor.execute(SELECT_SALT, (salt, )):
             return cursor.fetchone()
 
-    @atomic
     def get_user_email(self, cursor, email):
         return cursor.execute(SELECT_USER_EMAIL, (email,)).fetchone()
 
-    @atomic
     def get_user_salt(self, cursor, email):
         return cursor.execute(SELECT_SALT_USER, (email, )).fetchone()[0]
